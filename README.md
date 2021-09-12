4ef1067d8cceff5391f18e2329db0aededf1963b
pausala <pau.sala@bideaavant.com>
2021-09-12 23:16:55 +0200

    [UPDATE] Add parameters

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # bash_spinner
-Simple script to add a spinner to bash scripts log activity
+Simple spinner for bash scripts
+
+## Usage
+./spinner.sh [ -p title_color ] [ -s spinner_color ] [ -t tick_color ] [ -r quoted_spinner_string ] PID [ action_title ]
+
+It takes a process PID and optional title to display and shows the title with a litle animation while process is running. When process stops a green tick apears near the title. 
+Can take 4 optional parameters to configure the title_color, the spinner_color, the tick_color and a quoted string representing spinner steps, some cool ones are:  
+-  "▵▹▿◃"  
+-  "△▷▽◁"   
+-  "·:⁖⁘⁙⁘⁖:"   
+-  "⬖⬗⬘⬙"  
+-  "⋮⋰⋯⋱"  
+
+## Examples
+sleep 2 & ./spinner.sh "$!" "Action 1"
+sleep 2 & ./spinner.sh -p green -s blue "$!" "Action 2" 
+sleep 2 & ./spinner.sh -p green -s blue -t lpurple "$!" "Action 3"
+sleep 2 & ./spinner.sh -p green -s blue -t lpurple -r "△▷▽◁" "$!" "Action 4"
+
+## Avaliable colors
+black, red, green, orange, blue, purple, cyan, lgray, dgray, lred, lgreen, yellow, lblue, lpurple, lcyan and white
+
+That are mapped to standard bash colors: 
+Black        0;30     Dark Gray     1;30
+Red          0;31     Light Red     1;31
+Green        0;32     Light Green   1;32
+Brown/Orange 0;33     Yellow        1;33
+Blue         0;34     Light Blue    1;34
+Purple       0;35     Light Purple  1;35
+Cyan         0;36     Light Cyan    1;36
+Light Gray   0;37     White         1;37
--- spinner.sh
+++ spinner.sh
@@ -3,6 +3,43 @@
 # Spinner for bash 
 # [Some command] ./spinner.sh "$!" [command description]
 
+#COLORS
+declare -A COLORS=( 
+["black"]="\033[0;30m"    
+["red"]="\033[0;31m"       
+["green"]="\033[0;32m"      
+["orange"]="\033[0;33m"      
+["blue"]="\033[0;34m"      
+["purple"]="\033[0;35m"      
+["cyan"]="\033[0;36m"      
+["lgray"]="\033[0;31m"      
+["dgray"]="\033[1;30m" 
+["lred"]="\033[1;31m" 
+["lgreen"]="\033[1;32m" 
+["yellow"]="\033[1;33m" 
+["lblue"]="\033[1;34m" 
+["lpurple"]="\033[1;35m" 
+["lcyan"]="\033[1;36m" 
+["white"]="\033[1;37m" )
+
+#DEFAULT CONFIG
+PRIMARYC="${COLORS[yellow]}"
+SECONDARYC="${COLORS[cyan]}"
+TICKC="${COLORS[green]}"
+#sp="▵▹▿◃" sp="△▷▽◁" sp="·:⁖⁘⁙⁘⁖:" sp="⬖⬗⬘⬙"
+SPINNER="⋮⋰⋯⋱"
+
+# Function: Print a help message.
+usage() {                                       
+  echo "Usage: $0 [ -p title_color ] [ -s spinner_color ] [ -t tick_color ] [ -r quoted_spinner_string ] PID [ action_title ]" 1>&2 
+}
+# Function: Exit with error.
+exit_abnormal() {                         
+  usage
+  exit 1
+}
+
+# Function: Show and hide cursor.
 show_cursor() {
     tput cnorm
     exit
@@ -11,31 +48,45 @@ hide_cursor() {
     tput civis
 }
 
-CYAN="\033[1;36m"
-GREEN="\033[1;32m"
-YELLOW="\033[1;33m"
+# Function: Check if color is valid.
+check_valid_color() {
+    if [[ ${COLORS[$1]} ]]; 
+    then true
+    else exit_abnormal 
+    fi    # Exists
+}
 
-pid=$1
-title=$2
+#Get user options
+while getopts "p:s:t:r:" flag; do
+case "$flag" in
+    p) if check_valid_color $OPTARG; then PRIMARYC="${COLORS[$OPTARG]}"     ;fi; ;;
+    s) if check_valid_color $OPTARG; then SECONDARYC="${COLORS[$OPTARG]}"   ;fi; ;;
+    t) if check_valid_color $OPTARG; then TICKC="${COLORS[$OPTARG]}"        ;fi; ;;
+    r) SPINNER=$OPTARG;;
+esac
+done
 
-i=1
-#sp="▵▹▿◃" sp="△▷▽◁" sp="·:⁖⁘⁙⁘⁖:" sp="⬖⬗⬘⬙"
-sp="⋮⋰⋯⋱"
+PID=${@:$OPTIND:1}
+TITLE=${@:$OPTIND+1:1}
+#pid is mandatory
+if ! [[ $PID ]]; then exit_abnormal; fi
+
 
 #HIDE CURSOR
 trap show_cursor INT TERM
 hide_cursor
 
-while [ -d /proc/$pid ]
+i=1
+while [ -d /proc/$PID ]
 do
     for i in 1 2 3 4 
         do
-            echo -ne "${YELLOW}$title ${CYAN}${sp:i++%${#sp}:1}" "\r"; 
+            echo -ne "${PRIMARYC}$TITLE ${SECONDARYC}${SPINNER:i++%${#SPINNER}:1}" "\r"; 
             sleep .1;
     done
 done
 
-echo -ne "${YELLOW}$title ${GREEN}\xE2\x9C\x94"; 
+echo -ne "${PRIMARYC}$TITLE ${TICKC}\xE2\x9C\x94"; 
 echo " "
 
 show_cursor

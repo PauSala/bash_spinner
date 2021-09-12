b0ed771a795ed885b491cb58c6c7a8a2ef3cce8c
pausala <pau.sala@bideaavant.com>
2021-09-12 23:44:53 +0200

    update examples

--- test_spinner.sh
+++ test_spinner.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
-#sleep 2 & ./spinner.sh "$!" "Action 1"
-#sleep 2 & ./spinner.sh -p green -s blue "$!" "Action 2" 
-#sleep 2 & ./spinner.sh -p green -s blue -t lpurple "$!" "Action 3"
-#sleep 2 & ./spinner.sh -p green -s blue -t lpurple -r "△▷▽◁" "$!" "Action 4"
-
 sleep 2 & ./spinner.sh "$!" "Action 1"
-sleep 2 & ./spinner.sh "$!" "Action 2"
-sleep 2 & ./spinner.sh "$!" "Action 3"
-sleep 2 & ./spinner.sh "$!" "Action 4"
-sleep 2 & ./spinner.sh "$!" "Action 5"
+sleep 2 & ./spinner.sh -p green -s blue "$!" "Action 2" 
+sleep 2 & ./spinner.sh -p green -s blue -t lpurple "$!" "Action 3"
+sleep 2 & ./spinner.sh -p green -s blue -t lpurple -r "△▷▽◁" "$!" "Action 4"
+

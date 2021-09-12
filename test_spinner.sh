87b00dbb4b06f64ba8bf4dcdead25e38130b2858
pausala <pau.sala@bideaavant.com>
2021-09-12 23:38:16 +0200

    [UPDATE] add sample gif

--- test_spinner.sh
+++ test_spinner.sh
@@ -4,8 +4,8 @@
 #sleep 2 & ./spinner.sh -p green -s blue -t lpurple "$!" "Action 3"
 #sleep 2 & ./spinner.sh -p green -s blue -t lpurple -r "△▷▽◁" "$!" "Action 4"
 
-sleep 4 & ./spinner.sh "$!" "Action 1"
-sleep 4 & ./spinner.sh "$!" "Action 2"
-sleep 4 & ./spinner.sh "$!" "Action 3"
-sleep 4 & ./spinner.sh "$!" "Action 4"
-sleep 4 & ./spinner.sh "$!" "Action 5"
+sleep 2 & ./spinner.sh "$!" "Action 1"
+sleep 2 & ./spinner.sh "$!" "Action 2"
+sleep 2 & ./spinner.sh "$!" "Action 3"
+sleep 2 & ./spinner.sh "$!" "Action 4"
+sleep 2 & ./spinner.sh "$!" "Action 5"
